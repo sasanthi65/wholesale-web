e1b158172af200ec4a5c64bac49fa5e8730fafd4
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-14 23:30:24 +0530

    user registration error handling

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,47 +1,14 @@
-import userModel from "../models/userModel";
-import hashPassword from "./../helpers/authHelper.js";
+import userModel from "../models/userModel.js";
+
+import { hashPassword } from "./../helpers/authHelper.js";
 
 export const registerController = async (req, res) => {
   try {
-    const { name, email, password, phone, address, answer } = req.body;
-    //validations
-    if (!name) {
-      return res.send({ error: "Name is Required" });
-    }
-    if (!email) {
-      return res.send({ message: "Email is Required" });
-    }
-    if (!password) {
-      return res.send({ message: "Password is Required" });
-    }
-    if (!phone) {
-      return res.send({ message: "Phone no is Required" });
-    }
-    if (!address) {
-      return res.send({ message: "Address is Required" });
-    }
-    //register user
-    const hashedPassword = await hashPassword(password);
-    //save
-    const user = await new userModel({
-      name,
-      email,
-      phone,
-      address,
-      password: hashedPassword,
-      answer,
-    }).save();
-
-    res.status(201).send({
-      success: true,
-      message: "User Register Successfully",
-      user,
-    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Error in registration",
+      message: "Error in Registration",
       error,
     });
   }
