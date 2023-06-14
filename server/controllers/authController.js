e6243a893a89d1a1c63ccb254a01cc0f0ea545b4
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-14 23:48:27 +0530

    resolved conflicts in userModel.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,6 +4,48 @@ import { hashPassword } from "./../helpers/authHelper.js";
 
 export const registerController = async (req, res) => {
   try {
+    const { name, email, password, phone, address } = req.body;
+    //validations
+    if (!name) {
+      return res.send({ error: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ message: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ message: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ message: "Phone no is Required" });
+    }
+    if (!address) {
+      return res.send({ message: "Address is Required" });
+    }
+    //check user
+    const existingUser = await userModel.findOne({ email });
+    //existing user
+    if (existingUser) {
+      return res.status(200).send({
+        success: false,
+        message: "Already Registered please login",
+      });
+    }
+    //register user
+    const hashedPassword = await hashPassword(password);
+    //save
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword,
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "User Registered Successfully",
+      user,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -24,10 +24,6 @@ const userSchema = new mongoose.Schema(
       type: {},
       required: true,
     },
-    answer: {
-      type: String,
-      required: true,
-    },
     role: {
       type: Number,
       default: 0,
