6868b17de86554fca0f9ce771c176839cfab2e24
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-14 23:13:28 +0530

    user registration setup

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 //configure env
 dotenv.config();
@@ -17,6 +18,9 @@ const app = express();
 // app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
 
 //rest api
 app.get("/", (req, res) => {
