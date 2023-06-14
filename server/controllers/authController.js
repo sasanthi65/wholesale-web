244eb3f4a5ef1a18f6a51b46e674810f14dade7d
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-15 01:56:16 +0530

    error handling in login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -58,4 +58,14 @@ export const registerController = async (req, res) => {
 };
 
 //POST LOGIN
-export const loginController = () => {};
+export const loginController = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Login",
+      error,
+    });
+  }
+};
