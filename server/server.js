4b587835b71828e23fe758a711c8d3e1a6f2ab69
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-14 20:35:43 +0530

    mongo db setup and connection

--- server/server.js
+++ server/server.js
@@ -1,20 +1,35 @@
-const express = require("express");
-const colors = require("colors");
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
 
 // rest object
 const app = express();
 
-// rest api
+//middleware
+// app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//rest api
 app.get("/", (req, res) => {
-  res.send({
-    message: "Welcome to server side",
-  });
+  res.send("<h1>Welcome to Server Side</h1>");
 });
 
-//port
-const PORT = 8080;
+//PORT
+const PORT = process.env.PORT || 8080;
 
-// run app
+//run app
 app.listen(PORT, () => {
-  console.log(`Server is running ${PORT}`.bgCyan.white);
+  console.log(
+    `Server is running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
 });
