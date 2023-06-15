d219bb8c2c26bfffad938ecadb5c8ae17b373c32
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-15 23:09:48 +0530

    finalized footer component

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="bg-dark text-light p-3">
-      <h4 className="text-center">
+    <div className="footer">
+      <h3 className="text-center">
         All rights reserved &copy; Sasanthi Lakmini
-      </h4>
+      </h3>
+      <p className="text-center mt-3">
+        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+        <Link to="/policy">Privacy Policy</Link>
+      </p>
     </div>
   );
 };
