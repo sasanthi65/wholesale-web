bb419eb9b2507c5bb8e44f54e015e5ef63f736c7
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-15 15:31:41 +0530

    resolved name change error and added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import "./App.css";
-import Layout from "./components/Layout/layout";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
-      <Layout>
-        <h1>Retail Shop</h1>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,10 +1,52 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <NavLink to="/" className="navbar-brand" href="#">
+              Hidden brand
+            </NavLink>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink
+                  to="/"
+                  className="nav-link active"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link" href="#">
+                  Link
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link disabled">
+                  Disabled
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
