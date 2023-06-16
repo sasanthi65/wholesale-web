d09962d0e627b2fc117640ee6639b6111eb2715a
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-16 19:37:04 +0530

    setup for the client dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,14 @@ import Policy from "./pages/Policy";
 import PageNotFound from "./pages/PageNotFound";
 import { Register } from "./pages/Auth/Register";
 import { Login } from "./pages/Auth/Login";
+import Dashboard from "./pages/User/Dashboard";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
