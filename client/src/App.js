2168996a93d5c0795491a84555fc72c0cde71f50
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-17 20:28:54 +0530

    display products feature

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import CreateProduct from "./pages/Admin/CreateProduct";
 import Users from "./pages/Admin/Users";
 import Orders from "./pages/User/Orders";
 import Profile from "./pages/User/Profile";
+import Products from "./pages/Admin/Products";
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
           <Route path="admin" element={<AdminDashboard />} />
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/products" element={<Products />} />
           <Route path="admin/users" element={<Users />} />
         </Route>
         <Route path="/register" element={<Register />} />
