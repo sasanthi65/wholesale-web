fd3767954a7d32b879ee5d2a7bf848bb5e248c72
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-17 21:49:44 +0530

    fixed a typo

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -175,7 +175,7 @@ export const updateProductController = async (req, res) => {
     res.status(500).send({
       success: false,
       error,
-      message: "Error in Updte product",
+      message: "Error in Update product",
     });
   }
 };
