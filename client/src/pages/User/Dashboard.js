16b01fdb729677f2c468b5124265188197a11b89
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-19 00:00:48 +0530

    user profile setup

--- client/src/pages/User/Dashboard.js
+++ client/src/pages/User/Dashboard.js
@@ -7,7 +7,7 @@ const Dashboard = () => {
   const [auth] = useAuth();
   return (
     <Layout>
-      <div className="container-flui m-3 p-3 dashboard">
+      <div className="container-fluid m-3 p-3 dashboard">
         <div className="row">
           <div className="col-md-3">
             <UserMenu />
