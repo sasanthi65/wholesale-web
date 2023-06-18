4a862ae0f367530af9afa00dd01cdbeab05f833b
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-18 21:29:13 +0530

    get total of the items in cart

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -9,6 +9,22 @@ const CartPage = () => {
   const [cart, setCart] = useCart();
   const navigate = useNavigate();
 
+  //total price
+  const totalPrice = () => {
+    try {
+      let total = 0;
+      cart?.map((item) => {
+        total = total + item.price;
+      });
+      return total.toLocaleString("en-US", {
+        style: "currency",
+        currency: "LKR",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   //delete item
   const removeCartItem = (pid) => {
     try {
@@ -63,7 +79,12 @@ const CartPage = () => {
               </div>
             ))}
           </div>
-          <div className="col-md-4">Checkout | Payment </div>
+          <div className="col-md-4 text-center">
+            <h2>Cart Summary</h2>
+            <p>Total | Checkout | Payment</p>
+            <hr />
+            <h4>Total :{totalPrice()} </h4>
+          </div>
         </div>
       </div>
     </Layout>
