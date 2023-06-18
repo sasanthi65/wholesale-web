faf5e9b16eea2320a876a8d1b7779d390b84a34a
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2023-06-18 21:21:59 +0530

    remove items in cart,storing items in localStorage

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -8,6 +8,19 @@ const CartPage = () => {
   const [auth, setAuth] = useAuth();
   const [cart, setCart] = useCart();
   const navigate = useNavigate();
+
+  //delete item
+  const removeCartItem = (pid) => {
+    try {
+      let myCart = [...cart];
+      let index = myCart.findIndex((item) => item._id === pid);
+      myCart.splice(index, 1);
+      setCart(myCart);
+      localStorage.setItem("cart", JSON.stringify(myCart));
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Layout>
       <div className="container">
@@ -17,12 +30,40 @@ const CartPage = () => {
             {`Hello ${auth?.token && auth?.user?.name}`}
           </h1>
           <h4 className="text-center">
-            {cart?.length > 1
+            {cart?.length
               ? `You have ${cart.length} items in your cart ${
                   auth?.token ? "" : "Please Login to Checkout"
                 }`
               : "Your Cart is Empty"}
           </h4>
+        </div>
+        <div className="row">
+          <div className="col-md-8">
+            {cart?.map((p) => (
+              <div className="row mb-2 card p-3 flex-row">
+                <div className="col-md-4">
+                  <img
+                    src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
+                    className="card-img-top"
+                    alt={p.name}
+                  />
+                </div>
+                <div className="col-md-8">
+                  <p>{p.name}</p>
+                  <p>{p.description.substring(0, 40)}</p>
+                  <p>Price: {p.price}</p>
+                  <p>Qty: {p.quantity}</p>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeCartItem(p._id)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="col-md-4">Checkout | Payment </div>
         </div>
       </div>
     </Layout>
