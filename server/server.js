90dd3fef3cd5b110ba633d17887655e756cb1872
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2024-06-12 00:41:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,13 @@ connectDB();
 const app = express();
 
 //middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://wholesale-web.vercel.app"],
+    method:["POST","GET"],
+    credentials: true
+  }
+));
 app.use(express.json());
 app.use(morgan("dev"));
 
